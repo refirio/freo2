a1c95ddba64cb906b8981a62dfadf5b77df7717b
refirio <refirio@gmail.com>
2024-02-12 17:26:51 +0900

    RSS2.0の配信機能を実装

---
+++ app/views/entry/feed.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: text/xml; charset=' . MAIN_CHARSET);
+e('<?xml version="1.0" encoding="utf-8"?>' . "\n");
+?>
+<rss version="2.0" xml:lang="ja">
+    <channel>
+        <title><?php h($GLOBALS['setting']['title']) ?></title>
+        <link><?php h($GLOBALS['config']['http_url']) ?>/</link>
+        <description><?php h($GLOBALS['setting']['description']) ?></description>
+        <language>ja</language>
+        <lastBuildDate><?php h(localdate('D, d M Y H:i:s +0900', $_view['entries'][0]['datetime'])) ?></lastBuildDate>
+        <docs><?php h($GLOBALS['config']['http_url']) ?>/entry/feed</docs>
+        <?php foreach ($_view['entries'] as $entry) : ?>
+        <item>
+            <title><?php h($entry['title']) ?></title>
+            <link><?php h($GLOBALS['config']['http_url']) ?>/entry/detail/<?php h($entry['id']) ?></link>
+            <guid isPermaLink="true"><?php h($GLOBALS['config']['http_url']) ?>/entry/detail/<?php h($entry['id']) ?></guid>
+            <description><?php h(truncate(strip_tags($entry['text']), 200)) ?></description>
+            <pubDate><?php h(localdate('D, d M Y H:i:s +0900', $entry['datetime'])) ?></pubDate>
+        </item>
+        <?php endforeach ?>
+    </channel>
+</rss>
--- app/views/header.php
+++ app/views/header.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php isset($_view['title']) ? h($_view['title'] . ' | ') : '' ?><?php h($GLOBALS['setting']['title']) ?></title>
         <?php e($_view['widgets']['initial']) ?>
+        <link rel="alternate" href="<?php h($GLOBALS['config']['http_url']) ?>/entry/feed" type="application/rss+xml" title="RSS">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="<?php t($GLOBALS['config']['http_path']) ?><?php t(loader_css('common.css')) ?>">
         <link rel="stylesheet" href="<?php t($GLOBALS['config']['http_path']) ?><?php t(loader_css('pages.css')) ?>">
