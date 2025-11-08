022bf8d9c2b20fe9e5f899b8da91f6109344b7d4
refirio <refirio@gmail.com>
2025-11-08 14:42:33 +0900

    Ver 2.0.0-alpha-2

--- app/version.php
+++ app/version.php
@@ -1,4 +1,4 @@
 <?php
 
-define('APP_VERSION_NUMBER', '2.0.0-alpha-1');
-define('APP_VERSION_UPDATE', '2025-09-19');
+define('APP_VERSION_NUMBER', '2.0.0-alpha-2');
+define('APP_VERSION_UPDATE', '2025-11-08');
