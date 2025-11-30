95ae476f9ccfd86da1b71d6f1b2aa5633298bc00
refirio <refirio@gmail.com>
2025-11-30 18:13:37 +0900

    ダッシュボードに表示項目を追加

--- app/controllers/admin/index.php
+++ app/controllers/admin/index.php
@@ -9,6 +9,24 @@ $entry_count = model('select_entries', [
 ]);
 $_view['entry_count'] = $entry_count[0]['count'];
 
+// エントリー（未承認）数を取得
+$entry_not_approved_count = model('select_entries', [
+    'select' => 'COUNT(DISTINCT entries.id) AS count',
+    'where'  => 'types.code = ' . db_escape('entry') . ' AND entries.approved = 0',
+], [
+    'associate' => true,
+]);
+$_view['entry_not_approved_count'] = $entry_not_approved_count[0]['count'];
+
+// エントリー（非公開）数を取得
+$entry_public_none_count = model('select_entries', [
+    'select' => 'COUNT(DISTINCT entries.id) AS count',
+    'where'  => 'types.code = ' . db_escape('entry') . ' AND entries.public = ' . db_escape('none'),
+], [
+    'associate' => true,
+]);
+$_view['entry_public_none_count'] = $entry_public_none_count[0]['count'];
+
 // ページ数を取得
 $page_count = model('select_entries', [
     'select' => 'COUNT(DISTINCT entries.id) AS count',
@@ -18,6 +36,24 @@ $page_count = model('select_entries', [
 ]);
 $_view['page_count'] = $page_count[0]['count'];
 
+// ページ数（未承認）を取得
+$page_not_approved_count = model('select_entries', [
+    'select' => 'COUNT(DISTINCT entries.id) AS count',
+    'where'  => 'types.code = ' . db_escape('page') . ' AND entries.approved = 0',
+], [
+    'associate' => true,
+]);
+$_view['page_not_approved_count'] = $page_not_approved_count[0]['count'];
+
+// ページ数（非公開）を取得
+$page_public_none_count = model('select_entries', [
+    'select' => 'COUNT(DISTINCT entries.id) AS count',
+    'where'  => 'types.code = ' . db_escape('page') . ' AND entries.public = ' . db_escape('none'),
+], [
+    'associate' => true,
+]);
+$_view['page_public_none_count'] = $page_public_none_count[0]['count'];
+
 // お問い合わせ数を取得
 $contact_count = model('select_contacts', [
     'select' => 'COUNT(DISTINCT contacts.id) AS count',
@@ -25,3 +61,29 @@ $contact_count = model('select_contacts', [
     'associate' => true,
 ]);
 $_view['contact_count'] = $contact_count[0]['count'];
+
+// お問い合わせ数（未対応）を取得
+$contact_status_opened_count = model('select_contacts', [
+    'select' => 'COUNT(DISTINCT contacts.id) AS count',
+    'where'  => 'contacts.status = ' . db_escape('opened'),
+], [
+    'associate' => true,
+]);
+$_view['contact_status_opened_count'] = $contact_status_opened_count[0]['count'];
+
+// ユーザー数を取得
+$user_count = model('select_users', [
+    'select' => 'COUNT(DISTINCT users.id) AS count',
+], [
+    'associate' => true,
+]);
+$_view['user_count'] = $user_count[0]['count'];
+
+// ユーザー数（無効）を取得
+$user_enabled_count = model('select_users', [
+    'select' => 'COUNT(DISTINCT users.id) AS count',
+    'where'  => 'users.enabled = 0',
+], [
+    'associate' => true,
+]);
+$_view['user_enabled_count'] = $user_enabled_count[0]['count'];
--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -18,7 +18,7 @@
                                     <div class="card shadow-sm mb-3">
                                         <div class="card-header">エントリー数</div>
                                         <div class="card-body text-center">
-                                            <span class="fs-4"><a href="<?php t(MAIN_FILE) ?>/admin/entry"><?php h($_view['entry_count']) ?></a></span>
+                                            <span class="fs-4"><a href="<?php t(MAIN_FILE) ?>/admin/entry"><?php h($_view['entry_count']) ?></a></span><?php if ($GLOBALS['setting']['entry_use_approve']) : ?> / 未承認 <?php h($_view['entry_not_approved_count']) ?><?php else :?> / 非公開 <?php h($_view['page_not_approved_count']) ?><?php endif ?>
                                         </div>
                                     </div>
                                 </div>
@@ -26,7 +26,7 @@
                                     <div class="card shadow-sm mb-3">
                                         <div class="card-header">ページ数</div>
                                         <div class="card-body text-center">
-                                            <span class="fs-4"><a href="<?php t(MAIN_FILE) ?>/admin/page"><?php h($_view['page_count']) ?></a></span>
+                                            <span class="fs-4"><a href="<?php t(MAIN_FILE) ?>/admin/page"><?php h($_view['page_count']) ?></a></span><?php if ($GLOBALS['setting']['page_use_approve']) : ?> / 未承認 <?php h($_view['page_not_approved_count']) ?><?php else :?> / 非公開 <?php h($_view['page_public_none_count']) ?><?php endif ?>
                                         </div>
                                     </div>
                                 </div>
@@ -38,7 +38,18 @@
                                     <div class="card shadow-sm mb-3">
                                         <div class="card-header">お問い合わせ数</div>
                                         <div class="card-body text-center">
-                                            <span class="fs-4"><a href="<?php t(MAIN_FILE) ?>/admin/contact"><?php h($_view['contact_count']) ?></a></span>
+                                            <span class="fs-4"><a href="<?php t(MAIN_FILE) ?>/admin/contact"><?php h($_view['contact_count']) ?></a></span> / 未対応 <?php h($_view['contact_status_opened_count']) ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <h3 class="h5">システム</h3>
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="card shadow-sm mb-3">
+                                        <div class="card-header">ユーザー数</div>
+                                        <div class="card-body text-center">
+                                            <span class="fs-4"><a href="<?php t(MAIN_FILE) ?>/admin/user"><?php h($_view['user_count']) ?></a></span> / 無効 <?php h($_view['user_enabled_count']) ?>
                                         </div>
                                     </div>
                                 </div>
