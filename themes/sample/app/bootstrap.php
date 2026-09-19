ce2cbb2a73f797c34428028a50e18685c3cfbf72
cccabinet <chay84@hotmail.com>
2026-09-19 20:54:39 +0900

    サンプルテーマのお問い合わせタイトルを修正 (#3)

--- themes/sample/app/bootstrap.php
+++ themes/sample/app/bootstrap.php
@@ -6,5 +6,5 @@ $GLOBALS['string']['heading_entry_list'] = 'Entry';
 $GLOBALS['string']['heading_entry_recently'] = 'Entry';
 $GLOBALS['string']['heading_comment_list'] = 'Comment';
 $GLOBALS['string']['heading_comment_form'] = 'Comment Form';
-$GLOBALS['string']['heading_contact_form'] = 'Contact Form';
+$GLOBALS['string']['heading_contact'] = 'Contact Form';
 $GLOBALS['string']['heading_auth'] = 'Login';
